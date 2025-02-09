67aed4fe000061847744efd3c0a1c94eff464573
Sampathcode12 <lsampathlk4@gmail.com>
2025-02-09 17:53:44 +0530

    finishdender requst vew

--- fetch_Data.php
+++ fetch_Data.php
@@ -119,6 +119,28 @@ function fetchTenderData() {
     return ['data' => $response];
 }
 
+function fetchTenderApplyData() {
+    $url = 'http://localhost:5268/api/TenderAply/GetAllTenderProposal'; 
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    if (!$result) {
+        return ['error' => 'Failed to fetch data from API.'];
+    }
+
+    $response = json_decode($result, true);
+
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        return ['error' => 'Invalid JSON response from API.'];
+    }
+
+    return ['data' => $response];
+}
+
 ?>
 
 
---
+++ tenderAplicationView.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tender List</title>
+    <link rel="stylesheet" href="styles.css">  <!-- External CSS File -->
+</head>
+<body>
+
+<header>
+        <h2>Tender List</h2>
+    </header>
+  
+  
+
+    <table class="sTable">
+        <thead>
+            <tr>
+            <th>Suplier Id</th>
+                <th>Suplier Name</th>
+                <th>RF Number</th>
+                <th>Offer Price</th>
+                <th>Suplier Email</th>
+                <th>Proposal</th>
+                <th>Requcet Date</th>
+                
+        
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            include 'fetch_Data.php'; // Include PHP file with API call function
+            $TenderAplyData = fetchTenderApplyData();
+
+            if (isset($TenderAplyData['data'])) {
+                foreach ($TenderAplyData['data'] as $TenderAplication) {
+                    echo "<tr>
+                        <td>{$TenderAplication['aplicationID']}</td>
+                        <td>{$TenderAplication['supplierName']}</td>
+                        <td>{$TenderAplication['tenderRef']}</td>
+                        <td>{$TenderAplication['offeredPrice']}</td>
+                        <td>{$TenderAplication['supplierEmail']}</td>
+                        <td>{$TenderAplication['proposalDocument']}</td>   
+                         <td>{$TenderAplication['date']}</td>   
+              
+                     
+                      </tr>";
+
+                  
+                }
+            } else {
+                echo "<tr><td colspan='7'>No Proposal found or invalid response.</td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+<!-- 
+    <button onclick="window.location.href='Apliy_Tender.php';">Apply</button> -->
+    <button onclick="window.history.back();">Back</button>
+
+
+</body>
+</html>

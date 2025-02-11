9c7eaaf82a2ae46d83428dfa5a1b0b7f9f7a888f
Sampathcode12 <lsampathlk4@gmail.com>
2025-02-11 19:16:43 +0530

    link request drug form to in phamacy dashbord

---
+++ phamacyDashbord.php
@@ -0,0 +1,59 @@
+
+<?php
+
+include"Staff_header.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff - Home</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<!--  -->
+
+    <section class="welcome">
+        <h2>Welcome to SPC</h2>
+        <p>Your trusted partner in pharmaceutical supplies.</p>
+    </section>
+
+    <section class="features">
+        
+
+        
+        </div>
+
+        <div class="feature">
+            <h3> Drugs Request</h3>
+            <p>Submit and track tenders for pharmaceutical supplies.</p>
+            <a href="RequestDrugs.php" class="btn">Request</a>
+        </div>
+
+        <div class="feature">
+            <h3> avalable drugs</h3>
+            <p>Submit and track tenders for pharmaceutical supplies.</p>
+            <a href="tender_manage.php" class="btn">Request</a>
+        </div>
+
+        <div class="feature">
+            <h3> Chack  drugs</h3>
+            <p>Submit and track tenders for pharmaceutical supplies.</p>
+            <a href="tender_manage.php" class="btn">Request</a>
+        </div>
+
+    
+
+
+        
+      
+    </section>
+
+    <footer>
+        <p>&copy; 2024 SPC - State Pharmaceutical Cooperation</p>
+    </footer>
+</body>
+</html>

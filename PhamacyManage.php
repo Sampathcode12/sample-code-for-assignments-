64fdb3c8fed3ae9ec838624910f1a4f88bc0d98f
Sampathcode12 <lsampathlk4@gmail.com>
2025-02-11 19:13:46 +0530

    done create phamacy manage paje

---
+++ PhamacyManage.php
@@ -0,0 +1,77 @@
+<?php
+
+include"Staff_header.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff - Home</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+<body>
+ 
+
+    <section class="welcome">
+        <h2>Welcome to SPC</h2>
+        <p>Your trusted partner in pharmaceutical supplies.</p>
+    </section>
+
+    <section class="features">
+        
+
+        <div class="feature">
+            <h3>View Phamacys</h3>
+            <p>Search for available drugs and place orders easily.</p>
+            <a href="phamacyRequestView.php" class="btn">Staff Manage</a>
+        </div>
+
+        <div class="feature">
+            <h3> delete Phamacys </h3>
+            <p>Manage stock levels for manufactured and purchased drugs.</p>
+            <a href="PhamacyDelet.php" class="btn">Manage Suplier</a>
+        </div>
+
+        <div class="feature">
+            <h3>Update Phamacys</h3>
+            <p>Submit and track tenders for pharmaceutical supplies.</p>
+            <a href="PhamcyUpdate.php" class="btn">Manage Tenders</a>
+        </div>
+
+        <div class="feature">
+            <h3>Search Phamacys</h3>
+            <p>Staff can register and participate in tenders.</p>
+            <a href="PhamactDataSearch.php" class="btn">Register Now</a>
+        </div>
+
+
+        
+        <div class="feature">
+            <h3>view aPhamacy drug request</h3>
+            <p>Staff can register and participate in tenders.</p>
+            <a href="tenderAplicationView.php" class="btn">Register Now</a>
+        </div> 
+
+        <div class="feature">
+            <h3>view aPhamacy drug Conform data</h3>
+            <p>Staff can register and participate in tenders.</p>
+            <a href="tenderAplicationView.php" class="btn">Register Now</a>
+        </div> 
+      
+        <div class="feature">
+            <h3>view aPhamacy drug Reject data</h3>
+            <p>Staff can register and participate in tenders.</p>
+            <a href="tenderAplicationView.php" class="btn">Register Now</a>
+        </div> 
+    </section>
+
+    <footer>
+        <p>&copy; 2024 SPC - State Pharmaceutical Cooperation</p>
+    </footer>
+</body>
+</html>

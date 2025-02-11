ec053094763570ef35bc6fda1df7cd2b608847fe
Sampathcode12 <lsampathlk4@gmail.com>
2025-02-11 19:08:04 +0530

    no error in staff login page

--- staffLogin.php
+++ staffLogin.php
@@ -78,7 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Staff Login</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
